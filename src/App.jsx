7b8a82eba803a329400a5e536a16990c4640b914
KwameGH22 <adarkwa.y.k@gmail.com>
2023-11-10 11:06:18 +0000

    Assignment two of react track completed

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,29 @@ function App() {
 
   return (
     <>
-      <h1>Hello World</h1>
-      <p>I am Kwame Adarkwa-Yiadom, this is my first React application</p>
+      <div className="portfolio">
+        <h1 className="fname">Kwame Adarkwa-Yiadom</h1>
+        <div className="educ fname">
+          <h2>EDUCATIONAL BACKGROUND</h2>
+          <h3>Azubi Africa (July 2023 - March 2024)</h3>
+          <h4>SKILLS</h4>
+          <ul>HTML</ul>
+          <ul>CSS</ul>
+          <ul>JAVASCRIPT</ul>
+          <ul>REACTJS</ul>
+        </div>
+        <div className="educ fname">
+          <h2>PROFESSIONAL EXPERIENCE</h2>
+          <h3>Trainee Front-end Developer</h3>
+          <h4>Duties</h4>
+          <ul>
+            <li>Worked on building new Angular components for the customer-facing web app which improved the time on page for the average user by 3 minutes </li>
+            <li>Built extensive test coverage for all new features which decreased the number of customer complaints by 8%</li>
+            <li>Designed and developed landing pages and assets also guided designers with the best practices for designing a responsive website and email assets </li>
+            <li>Contributed to the in-house UI library to create reusable components that saved over 300 hours of development time per month</li>
+          </ul>
+        </div>
+      </div>
     </>
   )
 }
--- viteinstallation.md
+++ viteinstallation.md
@@ -4,8 +4,9 @@
 4. The package manager will provide you with options of frameworks to choose from.
 5. Select React and hit enter
 6. After selecting the react framework another prompt with options to choose from is invoked to select preffered variant.
-7. choose javascript and hit enter.
+7. Choose javascript and hit enter.
 8. After the installation runs a project directory with your preffered name is created.
-9. Change in the dirctory with the command cd project-name.
+9. Change in the directory with the command cd project-name.
 10. While in the project directory run the commmand npm install and wait for the required packages to be installed.
-11. After the installation run the command npm run dev. this opens a development server in your browser for previewing your vite-react application.
+11. After the installation run the command npm run dev. 
+12. This opens a development server in your browser for previewing your vite-react application.
